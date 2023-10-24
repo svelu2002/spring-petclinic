f5bf08aeeebef36922aea7a7f50e15925ac4fe66
Velu <svelu2002@gmail.com>
2023-10-24 11:05:33 +0530

    Initial commit

---
+++ spring-petclinic-angular/src/assets/env.js
@@ -0,0 +1,6 @@
+(function (window) {
+    window['env'] = window['env'] || {};
+  
+    // Environment variables
+    window['env']['REST_API_URL'] = 'http://localhost:9966/petclinic/api/';
+  })(this);
